31967ce1de0e3bb33974f63cf727103512e0b512
Kamil <kamgre7@gmail.com>
2023-10-01 22:55:49 +0200

    Added bootstrap

---
+++ src/bootstrap.ts
@@ -0,0 +1,41 @@
+import express, { Application } from 'express';
+import 'reflect-metadata';
+import 'express-async-errors';
+import { appConfig } from './config/appConfig';
+import { errorHandler } from './middlewares/errorHandler';
+import { fileToolsRouter } from './domains/fileTools/routes/fileToolsRouter';
+
+export class Bootstrap {
+  private readonly app: Application;
+
+  constructor() {
+    this.app = express();
+
+    this.configureMiddleware();
+    this.configureRoutes();
+    this.configureErrorHandler();
+    this.startServer();
+  }
+
+  private configureMiddleware(): void {
+    this.app.use(express.json());
+  }
+
+  private configureRoutes() {
+    this.app.use('/tool', fileToolsRouter);
+  }
+
+  private configureErrorHandler(): void {
+    this.app.use(errorHandler);
+  }
+
+  private startServer() {
+    this.app.listen(appConfig.port, () => {
+      console.log(
+        `Application is running on ${appConfig.host}:${appConfig.port}`
+      );
+    });
+  }
+}
+
+new Bootstrap();
