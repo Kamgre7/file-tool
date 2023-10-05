e6686a0e8112280257fefac0fb3e6aae060cdd00
Kamil <kamgre7@gmail.com>
2023-10-05 18:45:28 +0200

    Added update and delete methods

--- src/domains/fileTools/services/fileToolsService.ts
+++ src/domains/fileTools/services/fileToolsService.ts
@@ -7,7 +7,12 @@ import { ITextHandler } from '../textHandler/textHandler';
 
 export interface IFileToolsService {
   countPhrases(params: EditParams, file: FileInfo): number;
-  updatePhrases(params: EditParams, file: FileInfo): void;
+  updatePhrases(
+    params: EditParams,
+    file: FileInfo,
+    updatedPhrase: string
+  ): string;
+  deletePhrases(params: EditParams, file: FileInfo): string;
 }
 
 @injectable()
@@ -23,5 +28,19 @@ export class FileToolsService implements IFileToolsService {
     return this.textHandler.countPhrases(text, params);
   }
 
-  updatePhrases(params: EditParams, file: FileInfo): void {}
+  updatePhrases(
+    params: EditParams,
+    file: FileInfo,
+    updatedPhrase: string
+  ): string {
+    const text = file.buffer.toString();
+
+    return this.textHandler.update(text, params, updatedPhrase);
+  }
+
+  deletePhrases(params: EditParams, file: FileInfo): string {
+    const text = file.buffer.toString();
+
+    return this.textHandler.delete(text, params);
+  }
 }
--- src/domains/fileTools/textHandler/textHandler.ts
+++ src/domains/fileTools/textHandler/textHandler.ts
@@ -2,8 +2,11 @@ import 'reflect-metadata';
 import { injectable } from 'inversify';
 import { EditParams, Mode } from '../const';
 import { BadRequestError } from '../../../errors/badRequestError';
+import { regExpPatterns } from '../utils/utils';
 
 export interface ITextHandler {
+  update(text: string, params: EditParams, newPhrase: string): string;
+  delete(text: string, params: EditParams): string;
   countPhrases(text: string, params: EditParams): number;
   getLineOfText(text: string, line: number): string;
   splitByLines(text: string): string[];
@@ -12,6 +15,32 @@ export interface ITextHandler {
 @injectable()
 export class TextHandler implements ITextHandler {
   constructor() {}
+
+  update(text: string, params: EditParams, newPhrase: string): string {
+    const flags = params.mode === Mode.FIRST ? 'i' : 'gi';
+
+    const regExp = new RegExp(`\\b${params.phrase}\\b`, flags);
+
+    if (params.mode === Mode.LINE) {
+      const textByLines = this.splitByLines(text);
+
+      this.validateIfTextLinesGraterThanLine(textByLines.length, params.line!);
+
+      const textToChange = textByLines[params.line! - 1];
+
+      textByLines[params.line! - 1] = textToChange.replace(regExp, newPhrase);
+
+      return textByLines.join('\n');
+    }
+
+    return text.replace(regExp, newPhrase);
+  }
+
+  delete(text: string, params: EditParams): string {
+    const content = this.update(text, params, '');
+
+    return content.replace(regExpPatterns.removeMultipleSpaces, '');
+  }
 
   countPhrases(text: string, params: EditParams): number {
     this.validateIfNotFirstMode(params.mode);
@@ -29,15 +58,22 @@ export class TextHandler implements ITextHandler {
   getLineOfText(text: string, line: number): string {
     const textByLine = this.splitByLines(text);
 
-    if (textByLine.length < line) {
-      throw new BadRequestError('Invalid line');
-    }
+    this.validateIfTextLinesGraterThanLine(textByLine.length, line);
 
     return textByLine[line - 1];
   }
 
   splitByLines(text: string): string[] {
     return text.split('\n');
+  }
+
+  private validateIfTextLinesGraterThanLine(
+    textLines: number,
+    line: number
+  ): void {
+    if (textLines < line) {
+      throw new BadRequestError('Invalid line');
+    }
   }
 
   private validateIfNotFirstMode(mode: Mode): void {
