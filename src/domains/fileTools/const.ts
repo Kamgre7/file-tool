a29bd2c6751c9aa7dc1dc7582c218e76614f6af6
Kamil <kamgre7@gmail.com>
2023-10-08 23:03:40 +0200

    Added enum ModeWithoutFirst

--- src/domains/fileTools/const.ts
+++ src/domains/fileTools/const.ts
@@ -1,9 +1,10 @@
-import { EditFileReq } from './schemas/editFileSchema';
-
 export enum Mode {
   FIRST = 'first',
   ALL = 'all',
   LINE = 'line',
 }
 
-export type EditParams = EditFileReq['params'];
+export enum ModeWithoutFirst {
+  ALL = 'all',
+  LINE = 'line',
+}
